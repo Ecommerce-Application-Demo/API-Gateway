0aa781b9a71457791d0275d6475df13956d22429
Sourav Mazumder <122155642+souravM2000@users.noreply.github.com>
2024-03-31 12:06:25 +0530

    Enabled Scheduling

--- src/main/java/com/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/apigateway/ApiGatewayApplication.java
@@ -3,10 +3,12 @@ package com.apigateway;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableScheduling
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
